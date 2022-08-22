7a5f621e1231d559a84312b7172303f1e633e273
Brase <brasea@wwt.com>
2022-08-22 08:58:15 -0500

    adding new form component

---
+++ src/components/FormContact.js
@@ -0,0 +1,11 @@
+import './FormContact.css';
+
+const FormContact = () => {
+    return (
+        <div className='new-form'>
+            <form>test form</form>
+        </div>
+    );
+};
+
+export default FormContact;
